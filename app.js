941a8126f3c0eb147293634249ee5c40b10f6444
bigansh_ <anshpushti2001@gmail.com>
2021-04-22 18:59:51 +0530

    handle error event

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express'),
-    app = express(),
-    dotenv = require('dotenv')
+	app = express(),
+	dotenv = require('dotenv')
 
 const twit = require('./connections/twitterConnect')
 
@@ -9,39 +9,56 @@ const twitter = twit
 dotenv.config()
 
 const keyWords = [
-    '@CVDResourcesBot',
-    'beds available',
-    'oxygen available',
-    'remdesivir available',
-    '-needed -require -urgent',
-    'beds -needed -require -urgent -need',
-    'oxygen -needed -require -urgent -need',
-    'ventilator -needed -require -urgent -need',
-    'remdesivir -needed -require -urgent -need',
-    'oxygen verified',
-    'beds verified',
-    'ventilator verified',
-    'resource verified -needed -require -urgent -need'
+	'@CVDResourcesBot',
+	'beds verified',
+	'beds available',
+	'oxygen verified',
+	'oxygen available',
+	'ventilator verified',
+	'remdesivir available',
+	'-needed -require -urgent',
+	'beds -needed -require -urgent -need',
+	'oxygen -needed -require -urgent -need',
+	'remdesivir -needed -require -urgent -need',
+	'ventilator -needed -require -urgent -need',
+	'resource verified -needed -require -urgent -need',
 ]
 
 var stream = twitter.stream('statuses/filter', { track: keyWords })
 
-stream.on('tweet', async (tweet) => {
-    try {
-        if (tweet.quoted_status_id_str) {
-            twitter.post('statuses/retweet/:id', { id: tweet.quoted_status_id_str })
-        }
-        if (tweet.in_reply_to_status_id_str) {
-            twitter.post('statuses/retweet/:id', { id: tweet.in_reply_to_status_id_str })
-        }
-        else {
-            twitter.post('statuses/retweet/:id', { id: tweet.id_str })
-        }
-    } catch {
-        console.log(tweet)
-    }
+stream.on('tweet', (tweet) => {
+	if (tweet.quoted_status_id_str) {
+		twitter
+			.post('statuses/retweet/:id', {
+				id: tweet.quoted_status_id_str,
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}
+	if (tweet.in_reply_to_status_id_str) {
+		twitter
+			.post('statuses/retweet/:id', {
+				id: tweet.in_reply_to_status_id_str,
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	} else {
+		twitter
+			.post('statuses/retweet/:id', {
+				id: tweet.id_str,
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}
+})
+
+stream.on('error', (error) => {
+	console.log(error)
 })
 
 app.listen(process.env.PORT, () => {
-    console.log('connected')
+	console.log('connected')
 })
