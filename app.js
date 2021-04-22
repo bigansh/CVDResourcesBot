4d5783a9e103655eedd896e92552b1d509142cea
bigansh_ <anshpushti2001@gmail.com>
2021-04-22 17:31:19 +0530

    change keywords

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ dotenv.config()
 
 const keyWords = [
     '@CVDResourcesBot',
-    'resource verified',
+    'resource verified -need -require -urgent',
     'beds available',
     'oxygen available',
     'remdesivir available',
-    '-need',
-    '-require',
-    '-urgent'
+    'beds -need -require -urgent',
+    'oxygen -need -require -urgent',
+    'remdesivir -need -require -urgent',
 ]
 
 var stream = twitter.stream('statuses/filter', { track: keyWords })
